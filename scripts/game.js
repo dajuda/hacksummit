796b27bcccd697a23fd7ef0396b29bff000eb1ef
Eduardo Sigrist Ciciliato <eduardo.ciciliato@gmail.com>
2016-02-20 16:36:40 -0200

    First interaction

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,34 @@
-define([], function(){
+define([ 'jquery' ], function( $ ){
 
 	var self = {};
+
+	self.dataView = {
+		'play': {
+
+		},
+		'timeattack': {
+
+		},
+		'about': {
+
+		},
+	}
 
 	// Only to group initializing actions
 	function run() {
 		
 		console.log('game');
+		$('.main-menu-itens li').on('click', function( event ) {
+			event.preventDefault();
+			loadView($(this).attr('id'));
+		})
+	}
+
+	function loadView( template ) {
+		console.log(template);
+		require(['hbs!../templates/'+template], function( template ) {
+			$('#main-content').html(template(self.dataView[template]));
+		})
 	}
 
 	run();
