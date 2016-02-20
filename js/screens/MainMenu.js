bdc798eda17c75613b73ff1710434515f3882fbc
dajuda <cainan13@gmail.com>
2016-02-20 09:50:01 -0200

    Added and align menu buttons

--- js/screens/MainMenu.js
+++ js/screens/MainMenu.js
@@ -3,7 +3,9 @@ game.MainMenuScreen = me.ScreenObject.extend({
 	init: function( ) {
 		this._super(me.ScreenObject, 'init');
 		this.images = {
-			'play_button' : new me.ImageLayer(400, 350, { image: 'play_button', repeat: 'no-repeat' }),
+			'play_button' : new me.ImageLayer((me.game.viewport.width/2)-31.5, 350, { image: 'play_button', repeat: 'no-repeat' }),
+			'attack_button' : new me.ImageLayer(me.game.viewport.width/2-97.5, 400, { image: 'attack_button', repeat: 'no-repeat' }),
+			'about_button' : new me.ImageLayer(me.game.viewport.width/2-45, 450, { image: 'about_button', repeat: 'no-repeat' }),
 			'background': new me.ImageLayer(0, 0, { image: 'main_background' }),
 		};
 	},
@@ -12,6 +14,8 @@ game.MainMenuScreen = me.ScreenObject.extend({
 		console.log('Main menu reset args', arguments);
 		me.game.world.addChild(this.images.background);
 		me.game.world.addChild(this.images.play_button);
+		me.game.world.addChild(this.images.attack_button);
+		me.game.world.addChild(this.images.about_button);
 	},
 
 	onDestroyEvent: function( ) {
