dcd351598cdeae5bf89f61c53e8653d57b0fe327
Eduardo Sigrist Ciciliato <eduardo.ciciliato@gmail.com>
2016-02-20 09:31:47 -0200

    Adds play button and formated resourse

--- js/resources.js
+++ js/resources.js
@@ -1,11 +1,14 @@
 game.resources = [
 	// Sign Images
-	{ name: 'signs_cat', 		type: 'image', 	src: 'data/img/signs/catanimation.gif' },
-	{ name: 'main_background', 	type: 'image', 	src: 'data/img/bkg.png' },
-	{ name: 'ready_label', 	    type: 'image', 	src: 'data/img/ready_lbl.png' },
-	{ name: 'game_over_label', 	type: 'image', 	src: 'data/img/game_over_lbl.png' },
-	{ name: 'play_button', 	    type: 'image', 	src: 'data/img/play_btn.png' },
-	{ name: 'attack_button', 	type: 'image', 	src: 'data/img/attack_btn.png' },
-	{ name: 'about_button', 	type: 'image', 	src: 'data/img/about_btn.png' },
-	{ name: 'facebook_button', 	type: 'image', 	src: 'data/img/fb_btn.png' },
+	{ name: 'signs_cat', 		type: 'image', 	src: 'data/img/signs/catanimation.gif' 	},
+	// Backgrounds
+	{ name: 'main_background', 	type: 'image', 	src: 'data/img/bkg.png' 				},
+	// Labels
+	{ name: 'ready_label', 		type: 'image', 	src: 'data/img/ready_lbl.png' 			},
+	{ name: 'game_over_label', 	type: 'image', 	src: 'data/img/game_over_lbl.png' 		},
+	// Buttons
+	{ name: 'play_button', 		type: 'image', 	src: 'data/img/play_btn.png' 			},
+	{ name: 'attack_button', 	type: 'image', 	src: 'data/img/attack_btn.png' 			},
+	{ name: 'about_button', 	type: 'image', 	src: 'data/img/about_btn.png' 			},
+	{ name: 'facebook_button', 	type: 'image', 	src: 'data/img/fb_btn.png' 				},
 ]
--- js/screens/MainMenu.js
+++ js/screens/MainMenu.js
@@ -2,11 +2,16 @@ game.MainMenuScreen = me.ScreenObject.extend({
 
 	init: function( ) {
 		this._super(me.ScreenObject, 'init');
+		this.images = {
+			'play_button' : new me.ImageLayer(400, 350, { image: 'play_button', repeat: 'no-repeat' }),
+			'background': new me.ImageLayer(0, 0, { image: 'main_background' }),
+		};
 	},
 
 	onResetEvent: function( ) {
 		console.log('Main menu reset args', arguments);
-		me.game.world.addChild(new me.ImageLayer(0, 0, { image: 'main_background' }));
+		me.game.world.addChild(this.images.background);
+		me.game.world.addChild(this.images.play_button);
 	},
 
 	onDestroyEvent: function( ) {
