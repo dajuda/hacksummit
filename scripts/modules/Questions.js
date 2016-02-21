e732c9285e395df5f3e770a8587cc9e96109f8d1
dajuda <cainan13@gmail.com>
2016-02-21 08:34:14 -0300

    Added new questions

--- scripts/modules/Questions.js
+++ scripts/modules/Questions.js
@@ -1,7 +1,28 @@
 define( [], function(){
 
 	var questions = [
-		{ value: 'Today I went to the <strong>doctor</strong>', correct: 'doctor', url: 'data/img/signs/doctor.gif'},
+		{ value: 'Today I went to the <strong>doctor</strong>',             correct: 'doctor',      url: 'data/img/signs/doctor.gif'},
+		{ value: 'Leonardo DiCaprio is a great <strong>actor</strong>.',    correct: 'actor',       url: 'data/img/signs/actor.gif'},
+		{ value: "I'm proud to be an <strong>American</strong>.",           correct: 'american',    url: 'data/img/signs/american.gif'},
+		{ value: '<strong>Bat</strong> is scary.',                          correct: 'bat',         url: 'data/img/signs/bat.gif'},
+		{ value: '<strong>Cats</strong> are adorable.',                     correct: 'cat',         url: 'data/img/signs/cat.gif'},
+		{ value: 'Chocolate <strong>ice cream</strong> is great.',          correct: 'icecream',    url: 'data/img/signs/icecream.gif'},
+		{ value: 'The books <strong>on</strong> the table',                 correct: 'on',          url: 'data/img/signs/on.gif'},
+		{ value: 'Call the <strong>police</strong>!',                       correct: 'police',      url: 'data/img/signs/police.gif'},
+		{ value: 'Obama is the current US <strong>president.</strong>',     correct: 'president',   url: 'data/img/signs/president.gif'},
+		{ value: 'which is the <strong>program</strong> today?',            correct: 'program',     url: 'data/img/signs/program.gif'},
+		{ value: 'You got some <strong>progress</strongg>?',                correct: 'progress',    url: 'data/img/signs/progress.gif'},
+		{ value: 'The <strong>shower</strong> is working?',                 correct: 'shower',      url: 'data/img/signs/shower.gif'},
+		{ value: 'This <strong>singer</strogn> is great.',                  correct: 'singer',      url: 'data/img/signs/singer.gif'},
+		{ value: 'Please <strong>sit</strong> down.',                       correct: 'sit',         url: 'data/img/signs/sit.gif'},
+		{ value: '<strong>Sorry</strong> about that.',                      correct: 'sorry',       url: 'data/img/signs/sorry.gif'},
+		{ value: 'You <strong>speak</strong> well in public?',              correct: 'speak',       url: 'data/img/signs/speak.gif'},
+		{ value: 'Stay still',                                              correct: 'stay',        url: 'data/img/signs/stay.gif'},
+		{ value: "Let's use the <strong>step</strong>-by-<strong>step</strong>.", correct: 'step',  url: 'data/img/signs/step.gif'},
+		{ value: 'The books on the <strong>table</strong>',                 correct: 'table',       url: 'data/img/signs/table.gif'},
+		{ value: 'Please <strong>take</strong> this',                       correct: 'take',        url: 'data/img/signs/take.gif'},
+		{ value: '<strong>Wake up</strong> happy does very well',           correct: 'wake-up',     url: 'data/img/signs/wake-up.gif'},
+		{ value: 'Jequitibá is a beautiful <strong>tree</strong>.',         correct: 'tree',        url: 'data/img/signs/tree.gif'},
 	];
 
 	return questions;
