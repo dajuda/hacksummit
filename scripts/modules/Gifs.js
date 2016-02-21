755fc9502081c72986e1dd8fc6c571c7e9d8ce45
dajuda <cainan13@gmail.com>
2016-02-21 08:39:10 -0300

    Fix gif names.

--- scripts/modules/Gifs.js
+++ scripts/modules/Gifs.js
@@ -1,7 +1,7 @@
 define( [ ], function( ) {
 	
 	gifs = [
-		{ url: 'data/img/signs/catanimation.gif',   name: 'cat' },
+		{ url: 'data/img/signs/cat.gif',            name: 'cat' },
 		{ url: 'data/img/signs/actor.gif',          name: 'actor' },
 		{ url: 'data/img/signs/american.gif',       name: 'american' },
 		{ url: 'data/img/signs/bat.gif',            name: 'bat' },
@@ -12,14 +12,14 @@ define( [ ], function( ) {
 		{ url: 'data/img/signs/president.gif',      name: 'president' },
 		{ url: 'data/img/signs/program.gif',        name: 'program' },
 		{ url: 'data/img/signs/progress.gif',       name: 'progress' },
-		{ url: 'data/img/signs/showeranimation.gif',name: 'showeranimation' },
+		{ url: 'data/img/signs/shower.gif',         name: 'shower' },
 		{ url: 'data/img/signs/singer.gif',         name: 'singer' },
 		{ url: 'data/img/signs/sit.gif',            name: 'sit' },
 		{ url: 'data/img/signs/sorry.gif',          name: 'sorry' },
 		{ url: 'data/img/signs/stay.gif',           name: 'stay' },
 		{ url: 'data/img/signs/speak.gif',          name: 'speak' },
 		{ url: 'data/img/signs/step.gif',           name: 'step' },
-		{ url: 'data/img/signs/tableanimation.gif', name: 'tableanimation' },
+		{ url: 'data/img/signs/table.gif',          name: 'table' },
 		{ url: 'data/img/signs/take.gif',           name: 'take' },
 		{ url: 'data/img/signs/tree.gif',           name: 'tree' },
 		{ url: 'data/img/signs/wake-up.gif',        name: 'wake-up' },
