c05de3919c3d00290abdb4d267bdc7bb7e3a0f8a
Eduardo Sigrist Ciciliato <eduardo.ciciliato@gmail.com>
2016-02-20 23:42:04 -0300

    Adds the correct answer to the gif list

--- scripts/modules/Play.js
+++ scripts/modules/Play.js
@@ -5,14 +5,21 @@ define( [ 'jquery', 'app/modules/Sign' ], function( $, Sign ){
 		var self = this;
 		this._parent = parent;
 		this.templateData = {};
+		this.nGifs = 3;
 
 		this.templateLoaded = function( ) {
 			console.log('Play template Loaded');
 		};
 
 		this.loadView = function( container ) {
-			self.templateData.gifs = Sign.getRandomGifs(3);
 			self.templateData.question = Sign.getRandomQuestion();
+			self.templateData.gifs = Sign.getRandomGifs(2, self.templateData.question.correct);
+			
+			self.correct_gif = { name: self.templateData.question.correct, url: self.templateData.question.url };
+
+			var rPos = Math.floor(Math.random() * self.nGifs);
+			self.templateData.gifs.splice( rPos, 0, self.correct_gif);
+
 			for( i in self.templateData.gifs ) {
 				Sign.loadImage(gifs[i].url);
 			}
@@ -21,7 +28,6 @@ define( [ 'jquery', 'app/modules/Sign' ], function( $, Sign ){
 
 		this.loadTemplate = function( container ) {
 			require(['hbs!../templates/play'], function( template ) {
-				console.log(self.templateData);
 				$('#main-content').html(template(self.templateData));
 				$('#back_button').on('click', function( event ) {
 					event.preventDefault();
