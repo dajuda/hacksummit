3b4215443d5d7e32fa7f539f474b397b073aa8d8
Eduardo Sigrist Ciciliato <eduardo.ciciliato@gmail.com>
2016-02-20 22:19:59 -0200

    Play screen design changes

--- scripts/modules/Play.js
+++ scripts/modules/Play.js
@@ -2,19 +2,21 @@ define( [ 'app/modules/Sign' ], function( Sign ){
 
 	function Play( ) {
 
+		this.templateData = {};
+
 		this.templateLoaded = function( ) {
 			console.log('Play template Loaded');
 		};
 
 		this.loadView = function( container ) {
 			var gifs = Sign.getRandomGifs(3);
-			console.log(gifs);
+			this.loadTemplate( container );
 		};
 
 		this.loadTemplate = function( container ) {
 			require(['hbs!../templates/play'], function( template ) {
 				$('#main-content').html(template(this.templateData));
-				this.templateLoaded();
+				//this.templateLoaded();
 			});
 		};
 
